d992c51a1f535fcdd0d0d54cda75437926c050ad
Tadese10 <you@example.com>
2021-01-29 18:11:38 +0000

    Followed clean architecture

---
+++ CleanArchitecture/app/src/main/java/com/example/cleanarchitecture/business/test.java
@@ -0,0 +1,4 @@
+package com.example.cleanarchitecture.business;
+
+public class test {
+}
--- CleanArchitecture/buildSrc/src/main/java/dependencies/Versions.kt
+++ CleanArchitecture/buildSrc/src/main/java/dependencies/Versions.kt
@@ -1,12 +1,12 @@
 package dependencies
 
 object Versions {
-    val gradle = "3.5.3"
+    val gradle = "4.1.0"
     val build_tools =  "30.0.1"
     val compilesdk = 30
     val minsdk = 21
     val targetsdk = 30
-    val kotlin = "1.3.61"
+    val kotlin = "1.3.72"
     val ktx = "1.2.0"
     val dagger = "2.25.4"
     val nav_components = "2.3.0-alpha06"
